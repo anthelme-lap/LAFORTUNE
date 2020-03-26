4cc9e596fad63c769159014027faebf91c9b15bf
anthelme-lap <anthelmekoffi77@gmail.com>
2020-03-26 23:57:36 +0000

    modification

--- script.js
+++ script.js
@@ -6,14 +6,26 @@ let totalWin= document.querySelector("#totalGagne");
 let Gagne = document.querySelector("#SommeGagne");
 let Mise = document.querySelector("#SommeMise");
 let btn = document.querySelector("#boutton");
-
-function Rouetourne()
-{
-    let deg = Math.floor(Math.random() * 5000);
-    argentAleatoire(deg)
+btn.addEventListener("click", function(){
+    Rouetourne();
+})
+// function Rouetourne()
+// {
+//     let deg = Math.floor(Math.random() * 5000);
+    
+//     ima.style.transition= "all 5s ease-out";
+//     ima.style.transform = "rotate("+ deg + "deg)";
+//     console.log(deg)
+// }
+// Rouetourne();
+function Rouetourne(){
+    let deg = aleatoire(1000, 4000);
     ima.style.transition= "all 5s ease-out";
     ima.style.transform = "rotate("+ deg + "deg)";
+    console.log(deg)
 }
-Rouetourne();
-
+function aleatoire(min, max){
+    return parseInt(Math.random()*(min+max-1)+min)
+}
+Rouetourne()
 
